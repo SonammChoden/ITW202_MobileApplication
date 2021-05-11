38c282328d7de7303bad0b9844aa9ce50dc0da19
SonammChoden <csonam165@gmail.com>
2021-05-11 17:47:17 +0600

    ToDo14II

---
+++ ToDo14II/app/src/main/java/com/example/todo14ii/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.todo14ii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Alert(View view){
+        AlertDialog.Builder myAlertBuilder = new AlertDialog.Builder(MainActivity.this);
+        myAlertBuilder.setTitle("Alert");
+        myAlertBuilder.setMessage("Click ok to continue, or Cancel to stop:");
+        myAlertBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"Pressed Ok",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        myAlertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+             Toast.makeText(getApplicationContext(),"Pressed cancel",
+                     Toast.LENGTH_SHORT).show();
+            }
+        });
+        myAlertBuilder.show();
+    }
+}
