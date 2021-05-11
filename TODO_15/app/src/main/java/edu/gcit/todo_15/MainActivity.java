1bb2482561a69b53f9277d95e3f4fd381787c6ef
SonammChoden <csonam165@gmail.com>
2021-05-11 17:48:57 +0600

    ToDo15

---
+++ TODO_15/app/src/main/java/edu/gcit/todo_15/MainActivity.java
@@ -0,0 +1,54 @@
+package edu.gcit.todo_15;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+
+import com.google.android.material.tabs.TabLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("clock"));
+        tabLayout.addTab(tabLayout.newTab().setText("timer"));
+        tabLayout.addTab(tabLayout.newTab().setText("stopWatch"));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //to like the viewpager
+        ViewPager viewPager = findViewById(R.id.viewPage);
+        PageAdaptor adaptor = new PageAdaptor(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adaptor);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+}
