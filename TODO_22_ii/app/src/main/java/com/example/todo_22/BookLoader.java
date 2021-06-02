687c2955edb072478f15aa5a7646760ad85ea256
SonammChoden <csonam165@gmail.com>
2021-06-03 00:31:06 +0600

    AsyncTaskLoader

---
+++ TODO_22_ii/app/src/main/java/com/example/todo_22/BookLoader.java
@@ -0,0 +1,34 @@
+package com.example.todo_22;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+    private String mQueryString;
+
+    BookLoader (Context context, String queryString){
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+    //constructor
+    public BookLoader(@NonNull Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onStartLoading() {
+
+        super.onStartLoading();
+        forceLoad();
+    }
+
+}
